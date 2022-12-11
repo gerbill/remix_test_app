dc6708ce19683cd72bc7b3626cfef8d8bbd6764f
gerbill <s@oxton.ru>
2022-12-12 02:52:24 +0300

    fix

--- reload.sh
+++ reload.sh
@@ -2,4 +2,4 @@
 git pull
 docker rm -f remix_test_app
 docker build -t remix_test_app .
-docker run -d --name remix_test_app remix_test_app
+docker run -d -p 3333:3000 --name remix_test_app remix_test_app
